8103ecb0b3c890f5e0ce8a92d1c376f683f7142e
cherry-kiwi <hmongsangga@gmail.com>
2023-06-04 18:19:45 +0900

    아직 덜함
    우수범이 다른거 먼저 하라고 함

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     CharacterController character;
+    Animator anim;
 
     public float Speed = 5;
     float mouseX;
@@ -15,6 +16,8 @@ public class PlayerController : MonoBehaviour
         //마우스 커서를 보이지 않게 설정, 현재 위치에 고정시킴
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Start()
@@ -27,14 +30,26 @@ public class PlayerController : MonoBehaviour
         PlayerMovement();
         mouseX += Input.GetAxis("Mouse X") * 10; //마우스 좌우
         transform.eulerAngles = new Vector3(0, mouseX, 0);
+
     }
 
     void PlayerMovement()
     {
         float moveX = Input.GetAxis("Horizontal"); // 가로축
         float moveZ = Input.GetAxis("Vertical"); // 세로축
-
         Vector3 move = new Vector3(moveX, 0, moveZ);
-        character.Move(transform.TransformDirection(move) * Time.deltaTime * 10);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            character.Move(transform.TransformDirection(move) * Time.deltaTime * 15);
+            anim.SetBool("isRun", true);
+            anim.SetBool("isWalk", false);
+        }
+        else
+        {
+            character.Move(transform.TransformDirection(move) * Time.deltaTime * 10);
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isRun", false);
+        }
     }
 }
